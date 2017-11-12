18de26a9aa3d613ccb9a6e83f9929b7284d2c5da
taravanis <aravanis_t@yahoo.com>
2017-11-12 15:14:52 -0500

    add product view controller, collection cell.  Add image, description, and price

---
+++ CoderSwag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by Theophilos Aravanis on 11/12/17.
+//  Copyright © 2017 Theophilos Aravanis. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImag: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
